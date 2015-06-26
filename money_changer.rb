c82e9b0f575e3c3fc67cb45505bae1e9dcab4a0e
Jay Wengrow <jaywngrw@gmail.com>
2015-06-26 11:28:35 -0500

    testing for 1

--- money_changer.rb
+++ money_changer.rb
@@ -3,13 +3,16 @@ require 'rspec'
 class ChangeMachine
 
   def change(cents)
-
+    
   end
 end
 
 
 RSpec.describe ChangeMachine do
   describe '#change' do
-    
+    it 'should return [1] when given 1' do
+      machine = ChangeMachine.new
+      expect(machine.change(1)).to eq([1])
+    end
   end
 end
